48262935d1b287dc81512dfc25740aaad6a57dce
AnushkaDissanayake <anushkamadusanka5@gmail.com>
2022-11-01 18:33:14 +0530

    Load all items

--- src/main/java/lk/ijse/dep9/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ItemServlet.java
@@ -1,16 +1,72 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.ijse.dep9.dto.CustomerDTO;
+import lk.ijse.dep9.dto.ItemDTO;
 import lk.ijse.dep9.util.HttpServlet2;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "ItemServlet", value = "/items/*")
 public class ItemServlet extends HttpServlet2 {
+    @Resource(lookup ="java:/comp/env/jdbc/dep9-pos")
+    private DataSource pool;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String query = request.getParameter("q");
+        String size = request.getParameter("size");
+        String page = request.getParameter("page");
+
+        if (request.getPathInfo() == null || request.getPathInfo() == "/"){
+            loadAllItems(response);
+        } else if (query != null || size != null || page != null) {
+            //search paginate items
+        } else {
+            Matcher matcher = Pattern.compile("^/[a-fA-F-0-9]{8}(-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12}/?$").matcher(request.getPathInfo());
+            if (matcher.matches()){
+               //get item details
+            }
+        }
+    }
+
+    private void loadAllItems(HttpServletResponse response) {
+        try {
+            Connection connection = pool.getConnection();
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM item");
+
+            ArrayList<ItemDTO> items = new ArrayList<>();
+            while(rst.next()){
+                String code = rst.getString("code");
+                String description = rst.getString("description");
+                double unitPrice = rst.getDouble("unit_price");
+                int stock= rst.getInt("stock");
+                ItemDTO dto = new ItemDTO(code, description, unitPrice,stock);
+                items.add(dto);
+            }
+
+            connection.close();
+
+            Jsonb jsonb = JsonbBuilder.create();
+            response.setContentType("application/json");
+            jsonb.toJson(items, response.getWriter());
+
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -9,3 +9,14 @@ INSERT INTO customer(id, name, address) VALUES (UUID(), 'Dasun', 'Moratuwa'),
                                                (UUID(), 'Chathura', 'Moratuwa'),
                                                (UUID(), 'Tharindu', 'Moratuwa'),
                                                (UUID(), 'Anushka', 'Moratuwa');
+
+
+CREATE TABLE item(
+                     code VARCHAR(36) PRIMARY KEY ,
+                     description VARCHAR(350) NOT NULL ,
+                     unit_price DECIMAL(5,2) NOT NULL,
+                     stock INT NOT NULL
+);
+
+INSERT INTO item (code, description, unit_price,stock) VALUES (UUID(), 'Milk Powder', 700,100);
+INSERT INTO item (code, description, unit_price,stock) VALUES (UUID(), 'Egg', 50,200);
