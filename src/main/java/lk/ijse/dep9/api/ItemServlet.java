02f553fd0c67a2e70111dcfd145144581490329e
tharindudarsh <darshanaruh@gmail.com>
2022-11-01 16:42:47 +0530

    innitial-commit

--- src/main/java/lk/ijse/dep9/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ItemServlet.java
@@ -1,14 +1,17 @@
 package lk.ijse.dep9.api;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import lk.ijse.dep9.util.HttpServlet2;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
 @WebServlet(name = "ItemServlet", value = "/items/*")
 public class ItemServlet extends HttpServlet2 {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
