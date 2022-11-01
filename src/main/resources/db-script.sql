3bfd721e6832a0941a6a88b6c2526f89f3e98147
dasunrandil <dasunrandil@gmail.com>
2022-11-01 19:12:28 +0530

    update dbscript

--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -19,3 +19,22 @@ CREATE TABLE item(
 
 INSERT INTO item (code, description, unit_price,stock) VALUES (UUID(), 'Milk Powder', 700,100);
 INSERT INTO item (code, description, unit_price,stock) VALUES (UUID(), 'Egg', 50,200);
+
+CREATE TABLE `order`(
+                        id VARCHAR(36) PRIMARY KEY ,
+                        date DATE NOT NULL,
+                        customer_id VARCHAR(36) NOT NULL ,
+                        FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+INSERT INTO `order` (id, date, customer_id) VALUES (UUID(), DATE, 'ce8d9029-59e9-11ed-a3d6-709cd1433e10');
+
+CREATE TABLE orderDetail(
+                            order_id VARCHAR(36) NOT NULL ,
+                            item_code VARCHAR(36)  NOT NULL ,
+                            CONSTRAINT PRIMARY KEY (order_id, item_code),
+                            CONSTRAINT FOREIGN KEY (item_code) REFERENCES item(code),
+                            CONSTRAINT FOREIGN KEY (order_id) REFERENCES `order`(id),
+                            unit_price DECIMAL(5,2) NOT NULL ,
+                            quantity INT NOT NULL
+);
